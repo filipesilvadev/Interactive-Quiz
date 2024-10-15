7e2045f3f2d6591c4b4ef3cbbef304ca932e6868
Filipe Oliveira <filipesilva.dev@gmail.com>
2024-10-14 22:45:18 -0300

    adding scope variables

--- index.php
+++ index.php
@@ -67,6 +67,21 @@
                         </li>
                     </ul>
                 </div>
+                <div class="modulo azul">
+                    <h3>Tipos</h3>
+                    <ul>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=basico">
+                                Variáveis
+                            </a>
+                        </li>
+                        <li>
+                            <a href="exercicio.php?dir=variaveis&file=desafio_equacao">
+                                Desafio Equação
+                            </a>
+                        </li>
+                    </ul>
+                </div>
             </nav>
         </div>
     </main>
---
+++ variaveis/desafio_equacao.php
@@ -0,0 +1,15 @@
+<div class="titulo">Desafio Equação</div>
+
+<?php
+
+$numerador1 = ((6 * (3 + 2)) ** 2) / (3 * 2);
+
+$numerador2 = ((1 - 5) * (2-7) / 2) ** 2;
+
+$numeradorTotal = ($numerador1 - $numerador2) ** 3;
+
+$denominadorTotal = 10 ** 3;
+
+$total = $numeradorTotal / $denominadorTotal;
+
+echo $total;
